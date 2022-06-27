9596b3de4a3412bd5dfca68628670238e60cf4d2
Nikolay <nikolay.malygin@gmail.com>
2022-06-27 21:25:17 +0700

    init commit

---
+++ src/main/java/ru/malygin/wishlist_tb/WishlistTbApplication.java
@@ -0,0 +1,11 @@
+package ru.malygin.wishlist_tb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistTbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistTbApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/malygin/wishlist_tb/WishlistTbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.malygin.wishlist_tb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistTbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
